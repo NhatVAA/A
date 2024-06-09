742eea184f5acbd6e48a9ebd0e9f183aa6048087
Nhat <Dominhnhat77@gmail.com>
2024-06-09 17:17:12 +0700

    first commit

---
+++ config/app/user.php
@@ -0,0 +1,5 @@
+<?php
+    return [
+        'title' => 'Quản Lý Thành Viên'
+    ];
+?>
---
+++ resources/views/backend/dashboard/component/head.blade.php
@@ -0,0 +1,12 @@
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>CT-xe-dien | Admin </title>
+
+    <link href="../../resources/themeplace/css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="../../resources/themeplace/css/animate.css" rel="stylesheet">
+    <link href="../../resources/themeplace/css/style.css" rel="stylesheet">
+    <link href="../../resources/themeplace/css/customize.css" rel="stylesheet">
